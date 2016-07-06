1cdf374f07a4ad3ed9b98c0b8366acca89b6a77f
Morgan <emorga06@tufts.edu>
2016-07-06 17:08:29 -0400

    Deduplicated rows of dictionary file to keep only most recent (i.e. latest year) variable description, then merged back with compiled dictionary to keep other information; year is included to specify the file from which the definition has been pulled

--- dictionary compilation.R
+++ dictionary compilation.R
@@ -1,8 +1,18 @@
 ### create compiled multi year dictionaries to be used with Emma's code
+#ECM
+#7.6.2016
+#Code for compiling IPEDS dictionary files across years
+#Adjusted KMF code to merge into unduplicated file, taking variable title from most recent year
+#Adjustments are still needed to standardize and use for multiple purposes
+#Tufts University OIRE
+#emma.morgan@tufts.edu
+
 # KMF
 # 7.1.16
 # Smith College IR
 # kfoley@smith.edu
+
+
 
 ##This script assumes IPEDS "varlist" tabs from dictionary files have been saved as csv
 # and that the "vartitle" column has been renamed to include the year (i.e. "2008vartitle")
@@ -16,7 +26,7 @@ library(tidyr)
 library(reshape)
 
 #set wd as necessary for a given set of varlist csvs
-setwd("Q:/Staff/OIRE/Staff Folders/Emma/Peer Database/IPEDS/Graduations")
+setwd("Q:/Staff/OIRE/Staff Folders/Emma/Peer Database/IPEDS/Fall Enrollment A")
 
 #create empty list and read in all teh csvs in your wd
 ds_list <- list()
@@ -24,6 +34,7 @@ ds_list <- list()
 for (i in 1:length(list.files())) {
 
   workFile <- list.files()[i]
+  year <- substr(workFile,3,6)
   capture.output(sheets <- excel_sheets(workFile), file='NUL')
   sheets <- tolower(sheets)
   if ("varlist" %in% sheets) {
@@ -34,46 +45,20 @@ for (i in 1:length(list.files())) {
     sheetName <- readline(prompt="Enter the name of the sheet: ")
   }
   capture.output(ds <- read_excel(workFile,sheetName), file ='NUL')
+  ds[['year']] <- year
   names(ds) <- toupper(names(ds))
   ds_list[[i]] <- assign(paste("ds",i,sep=""),ds)
 }
 
 rm(ds)
 
+myall <- merge_all(ds_list)
+all_unduplicated <- unique(ddply(myall, .(VARNAME), function(x) data.frame(VARNAME=x[,"VARNAME"], 
+                                                    YEAR=max(x$YEAR))))
 
+final_all <- merge(all_unduplicated, myall, all=FALSE)  
 
-for (i in 1:length(list.files())){
-  ds <- read.csv(list.files()[i], check.names=FALSE)
-  colnames(ds)=gsub(".*\\.", "", colnames(ds))
-  ds_list [[i]]<- assign(paste("ds", i,sep=""), ds)
-  
-}
+#write compiled dictionary file to folder
 
-rm(ds)
-
-all <- merge_recurse(ds_list)
-
-  #Some years of data have lowercase headers, so change all to uppercase for consistency
-  names(ds1) <- toupper(names(ds1))
-  names(ds2) <- toupper(names(ds2))
-  names(ds3) <- toupper(names(ds3))
-  names(ds4) <- toupper(names(ds4))
-  names(ds5) <- toupper(names(ds5))
-  names(ds6) <- toupper(names(ds6))
-  names(ds7) <- toupper(names(ds7))
-
-  
-    
-# join all varlist files together -- outer join
-  
-all <- merge(ds1, ds2, by = intersect(names(ds1), names(ds2)), all.x=T, all.y=T) 
-all <- merge(all, ds3, by = intersect(names(all), names(ds3)), all.x=T, all.y=T)
-all <- merge(all, ds4, by = intersect(names(all), names(ds4)), all.x=T, all.y=T)
-all <- merge(all, ds5, by = intersect(names(all), names(ds5)), all.x=T, all.y=T)
-all <- merge(all, ds6, by = intersect(names(all), names(ds6)), all.x=T, all.y=T)
-all <- merge(all, ds7, by = intersect(names(all), names(ds7)), all.x=T, all.y=T)
-
-#write compiled dictionary file out to S drive
-
-write.csv(all, "/Volumes/files-1/Shared/IRO/DATA/WORK/IPEDS/IPEDS cleanup using R/Fall Enrollment/dictionaries/compiled dictionary/compiled.csv")
+write.csv(final_all, "Q:/Staff/OIRE/Staff Folders/Emma/Peer Database/IPEDS/Compiled Dictionary/efa_dictionary.csv")
 
