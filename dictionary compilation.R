6208ff3cd1c750f9fc31fc59309d3d7700e993d7
Morgan <emorga06@tufts.edu>
2016-07-06 14:02:57 -0400

    Adjusted and condensed code to be more streamline; additionally added merge_recurse function from reshape package to merge all data frames in a singel step

--- dictionary compilation.R
+++ dictionary compilation.R
@@ -8,10 +8,12 @@
 # and that the "vartitle" column has been renamed to include the year (i.e. "2008vartitle")
 
 # frequenlty used packages that I just always library in case I need them.  
+# reshape package must be loaded for merge_recurse
 library(plyr)
 library(dplyr)
 library(stringr)
 library(tidyr)
+library(reshape)
 
 #set wd as necessary for a given set of varlist csvs
 setwd("Q:/Staff/OIRE/Staff Folders/Emma/Peer Database/IPEDS/Graduations")
@@ -19,19 +21,37 @@ setwd("Q:/Staff/OIRE/Staff Folders/Emma/Peer Database/IPEDS/Graduations")
 #create empty list and read in all teh csvs in your wd
 ds_list <- list()
 
-i <- 1
+for (i in 1:length(list.files())) {
 
-sheets <-   excel_sheets(list.files()[i])
+  workFile <- list.files()[i]
+  capture.output(sheets <- excel_sheets(workFile), file='NUL')
+  sheets <- tolower(sheets)
+  if ("varlist" %in% sheets) {
+    sheetName <- "varlist"
+  }
+  else {
+    print(sheets)
+    sheetName <- readline(prompt="Enter the name of the sheet: ")
+  }
+  capture.output(ds <- read_excel(workFile,sheetName), file ='NUL')
+  names(ds) <- toupper(names(ds))
+  ds_list[[i]] <- assign(paste("ds",i,sep=""),ds)
+}
+
+rm(ds)
+
 
 
 for (i in 1:length(list.files())){
   ds <- read.csv(list.files()[i], check.names=FALSE)
-  colnames(ds)=gsub(".*//.", "", colnames(ds))
+  colnames(ds)=gsub(".*\\.", "", colnames(ds))
   ds_list [[i]]<- assign(paste("ds", i,sep=""), ds)
   
 }
 
 rm(ds)
+
+all <- merge_recurse(ds_list)
 
   #Some years of data have lowercase headers, so change all to uppercase for consistency
   names(ds1) <- toupper(names(ds1))
