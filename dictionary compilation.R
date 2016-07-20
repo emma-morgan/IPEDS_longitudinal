44381b49c9d07c44b0332d86eaf9a3cdf10a9f93
Morgan <emorga06@tufts.edu>
2016-07-20 09:41:02 -0400

    Add IPEDS merge code to folder

--- dictionary compilation.R
+++ dictionary compilation.R
@@ -27,39 +27,64 @@ library(reshape)
 library(readxl)
 
 #set wd as necessary for a given set of varlist csvs
-setwd("Q:/Staff/OIRE/Staff Folders/Emma/Peer Database/IPEDS/Fall Enrollment A")
+#setwd("Q:/Staff/OIRE/Staff Folders/Emma/Peer Database/IPEDS/Fall Enrollment A")
 
-#create empty list and read in all teh csvs in your wd
-ds_list <- list()
 
-for (i in 1:length(list.files())) {
+compile_IPEDS_dictionary <- function() {
+  #Choose working directory for your dictionary files
+  dictionary.dir <- choose.dir(caption="Select location of your dictionary files")
+  
+  #set working directory to focus on dictionary files
+  setwd(dictionary.dir)
+  
+  
+  #create empty list and read in all the csvs in your wd
+  ds_list <- list()
+  
+  for (i in 1:length(list.files())) {
+    
+    workFile <- list.files()[i]
+    year <- substr(workFile,3,6)
+    capture.output(sheets <- excel_sheets(workFile), file='NUL')
+    sheets <- tolower(sheets)
+    if ("varlist" %in% sheets) {
+      sheetName <- "varlist"
+    }
+    else {
+      print(sheets)
+      sheetName <- readline(prompt="Enter the name of the sheet: ")
+    }
+    capture.output(ds <- read_excel(workFile,sheetName), file ='NUL')
+    #Clean any blank rows that have been mistakenly imported
 
-  workFile <- list.files()[i]
-  year <- substr(workFile,3,6)
-  capture.output(sheets <- excel_sheets(workFile), file='NUL')
-  sheets <- tolower(sheets)
-  if ("varlist" %in% sheets) {
-    sheetName <- "varlist"
+    ds <- ds[!apply(is.na(ds) | ds =="",1,all),]
+    
+    ds[['year']] <- year
+    names(ds) <- toupper(names(ds))
+    ds_list[[i]] <- assign(paste("ds",i,sep=""),ds)
   }
-  else {
-    print(sheets)
-    sheetName <- readline(prompt="Enter the name of the sheet: ")
+  
+  rm(ds)
+  
+  myall <- merge_all(ds_list)
+  all_unduplicated <- unique(ddply(myall, .(VARNAME), function(x) data.frame(VARNAME=x[,"VARNAME"], 
+                                                                             YEAR=max(x$YEAR))))
+  
+  final_all <- merge(all_unduplicated, myall, all=FALSE)  
+  
+  #write compiled dictionary file to folder
+  #Select directory to export your compiled dictionary file
+  DatedFileYn <- readline(prompt="Would you like to save a dated file? (Y/N) ")
+  
+  dictName <- readline(prompt="Filename for compiled dictionary: ")
+  compiled_dict_dir <- choose.dir(caption="Select your folder for compiled dictionary files")
+#  setwd(compiled_dict_dir)
+  write.csv(final_all, paste(compiled_dict_dir,"\\",dictName,".csv",sep=""),na="",row.names=FALSE)
+  
+  if (tolower(DatedFileYn) == "y") {
+    datedDir <- choose.dir("Select your directory for dated files")
+    write.csv(final_all, paste(datedDir,"\\",dictName,"_",
+                               Sys.Date(),".csv",sep=""),na="",row.names=FALSE)
   }
-  capture.output(ds <- read_excel(workFile,sheetName), file ='NUL')
-  ds[['year']] <- year
-  names(ds) <- toupper(names(ds))
-  ds_list[[i]] <- assign(paste("ds",i,sep=""),ds)
+  return(final_all)
 }
-
-rm(ds)
-
-myall <- merge_all(ds_list)
-all_unduplicated <- unique(ddply(myall, .(VARNAME), function(x) data.frame(VARNAME=x[,"VARNAME"], 
-                                                    YEAR=max(x$YEAR))))
-
-final_all <- merge(all_unduplicated, myall, all=FALSE)  
-
-#write compiled dictionary file to folder
-
-write.csv(final_all, "Q:/Staff/OIRE/Staff Folders/Emma/Peer Database/IPEDS/Compiled Dictionary/efa_dictionary.csv")
-
