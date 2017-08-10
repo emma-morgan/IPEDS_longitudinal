b578f1c09dbb1d6cc3132de08be22565ea43e866
kaloisio <kaloisio@gmail.com>
2017-08-10 16:47:37 -0400

    turned it into a function - works with Fall Enroll A

--- add_valuesets.R
+++ add_valuesets.R
@@ -3,41 +3,45 @@
 #' KMA - 8/10/17
 
 #' subset valueset to only contain variables from the lonitudinal table 
+# XXX SHOULD THIS BE IN THIS FUNCTION??
 
-# there should exist code to do this for now used this:
 
-valueset_EFA <- valuesets %>% 
-  filter(Tablenumber==21,
-         varName%in%c("EFALEVEL", "LINE", "SECTION", "LSTUDY"))
-
-ds <- longtable %>%
-
-#'because some institutions have multiple values in one value set for same year need to add a unique row id for spread to work XX SHOULD THIS HAPPEN HERE????
-  mutate(row_id = 1:nrow(longtable)) %>% 
-
-#' select only the variables in small valueset to gather
+add_values <- function(longtable, valueset) {
   
-  gather("varName", "var_numeric", names(longtable)[names(longtable)%in%valueset_EFA$varName]) %>% 
+  ds <- longtable %>%
+  
+  #'because some institutions have multiple values in one value set for same year need to add a unique row id for spread to work XX SHOULD THIS HAPPEN HERE????
+   # mutate(row_id = 1:nrow(longtable)) %>% 
+  
+  #' select only the variables in small valueset to gather
+    
+    gather("varName", "Codevalue", names(longtable)[names(longtable)%in%valueset$varName]) %>% 
+  
+  #' left join long table to small valueset
+  
+    left_join(select(valueset,
+                     "varName", 
+                     "Codevalue", 
+                     "valueLabel")) %>% 
+  
+  #' paste variable value numeric with variable value description into a new column crazy separater hopefully never in description
+  
+    mutate(value_code_label = 
+             paste0(Codevalue, "/_/", valueLabel)) %>% 
+  #' remove Codevalue and valueLabel for spread to function correctly
+    select(-Codevalue, -valueLabel) %>% 
+  
+  #' spread on variable fill in with var number desc
+    spread(key = varName, value = value_code_label)
+  
+  #' separate the value from the description remove the original gross one
+  
+  for(name in names(ds)[names(ds)%in%valueset$varName]) ds <- separate_(ds, name,sep = "/_/", into = c(paste0(name, "_value"), name), remove = T)
 
-#' left join long table to small valueset
+  #' return dataset  
+  return(ds)
+}
 
-  left_join(ds, select(valueset_EFA,
-                       "varName", 
-                       "Codevalue", 
-                       "valueLabel")) %>% 
-
-#' paste variable value numeric with variable value description into a new column
-
-  mutate(value_code_label = 
-           paste0(Codevalue, "_", valueLabel)) %>% 
-#' remove Codevalue and valueLabel for spread to function correctly
-  select(-Codevalue, -valueLabel) %>% 
-
-#' spread on variable fill in with var number desc
-  spread(key = varName, value = value_code_label)
-
-#' separate the value from the description
-
-for(name in names(ds)[names(ds)%in%valueset_EFA$varName]) ds <- separate_(ds, name,sep = "/_/", into = c(name, paste0(name, '_desc')))
-
-#' return dataset
+#' testing function
+#' 
+test <- add_values(longtable = ds, valueset = valueset_EFA)
