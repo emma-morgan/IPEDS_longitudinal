3cb9bfbd76fb90df89b6d8cfb91484e2cb81ed64
Emma Morgan <emma.morgan@tufts.edu>
2017-08-11 10:36:06 -0400

    adding a column for within-table unique variable titles that can be replaced for usability in the compiled files

--- compile_varname_valueset.R
+++ compile_varname_valueset.R
@@ -29,19 +29,15 @@ ay_from_tablesdoc <- function(filename) {
     print.warnings(paste("One or more files is not a valid IPEDS_TablesDoc download:",filename,sep=" "))
   }
   else {
-    AcadYear <- paste(substr(filename,6,9),substr(filename,10,11),sep="-")
+    first_digit <- regexpr("\\d{6}",filename)[[1]]
+    AcadYear <- paste(substr(filename,first_digit,first_digit+3),substr(filename,first_digit+4,first_digit+5),sep="-")
   }
 }
 
 
-
-varfiles_list <- list()
-vartables_list <- list()
-valuesets_list <- list()
-
 #General function to read in a sheet from a file name given a path and a string we're looking for in the name
 get_sheet <- function(filepath,sheetname){
-  sheet_index <- which(grepl(sheetname,excel_sheets(filepath),ignore.case=TRUE))
+  sheet_index <- which(grepl(sheetname,readxl::excel_sheets(filepath),ignore.case=TRUE))
   if (length(sheet_index)==1) {sheet_df <- readxl::read_excel(path=filepath,sheet=sheet_index)}
   else {print.warnings(paste("The following file does not contain unique sheet ",sheetname,": ",filepath,sep=""))}
   return(sheet_df)
@@ -71,9 +67,11 @@ names(valuesets_list) <- AcadYear_list
 #   This could be incorporated when reading in the file, but I don't want to mess with "get_sheets" right now
 
 for (i in 1:length(AcadYear_list)) {
-  vartables_list[[i]]['AcadYear'] <- names(vartables_list)[[i]]
-  valuesets_list[[i]]['AcadYear'] <- names(valuesets_list)[[i]]
+  vartables_list[[i]]['ACADYEAR'] <- names(vartables_list)[[i]]
+  valuesets_list[[i]]['ACADYEAR'] <- names(valuesets_list)[[i]]
 }
+
+
 
 vartable_compiled <- dplyr::bind_rows(vartables_list)
 valuesets_compiled <- dplyr::bind_rows(valuesets_list)
@@ -84,9 +82,47 @@ valuesets_compiled[['valueset_id']] <- paste(valuesets_compiled[['varName']],val
 vartable_distinct <- vartable_compiled[!duplicated(vartable_compiled['variable_id'], fromLast = TRUE),]
 valuesets_distinct <- valuesets_compiled[!duplicated(valuesets_compiled['valueset_id'], fromLast = TRUE),]
 
+#Make column names all uppercase for CONSISTENCY
+names(vartable_distinct) <- toupper(names(vartable_distinct))
+names(valuesets_distinct) <- toupper(names(valuesets_distinct))
+
 #Choose directory to write out files; this can also be specified explicitly
 outputdir <- choose.dir()
+##For Emma:  outputdir <- "Q:\\Staff\\University-Wide\\Peer Comparison Database\\IPEDS\\Access Database\\Compiling var info\\R export"
 
 write.csv(vartable_distinct,paste(outputdir,"vartable_compiled_2.csv",sep="\\"), row.names=FALSE,na="")
 write.csv(valuesets_distinct,paste(outputdir,"valuesets_compiled2.csv",sep="\\"), row.names=FALSE,na="")
 
+
+
+#***************************************************************
+# Create unique vartitle in vartable compiled for our eventual lookup
+#***************************************************************
+
+#Start with a compiled vartable_distinct file
+
+vartable <- vartable_distinct
+
+#Get a list of the table numbers; these are what we need to go through
+list_of_tables <- names(table(vartable[['TABLENUMBER']]))
+
+var_by_table <- lapply(list_of_tables, function(x) vartable[which(vartable['TABLENUMBER']==x),])
+names(var_by_table) <- list_of_tables
+
+for (t in names(var_by_table)){
+  table_vars <- var_by_table[[t]]
+  duplicate_vartitles <- which(duplicated(table_vars['VARTITLE'], fromLast = FALSE) | duplicated(table_vars['VARTITLE'], fromLast = TRUE))
+  table_vars[['VARTITLE_USE']] <- table_vars[['VARTITLE']]
+  if (length(duplicate_vartitles) > 0 ){
+    table_vars[['VARTITLE_USE']][duplicate_vartitles] <- sapply(duplicate_vartitles, 
+                                                     function(x) table_vars[['VARTITLE_USE']][x] <- 
+                                                       paste(table_vars[['VARTITLE_USE']][x]," (",table_vars[['VARNAME']][x],")",sep=""))
+  }
+  var_by_table[[t]] <- table_vars
+}
+
+vartable_goodNames <- dplyr::bind_rows(var_by_table)
+
+outputdir <- "Q:\\Staff\\University-Wide\\Peer Comparison Database\\IPEDS\\Access Database\\Compiling var info\\R export"
+
+write.csv(vartable_goodNames,paste(outputdir,"vartitles_unique_within_table.csv",sep="\\"),row.names=FALSE,na="")
