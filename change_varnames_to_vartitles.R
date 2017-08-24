2d9ab9b361851adee28a38c9378bc2beb3903b6f
Emma Morgan <emma.morgan@tufts.edu>
2017-08-24 14:17:53 -0400

    Error running the varnmes to vartitles code; function could not find 'vars'; this seems to ahve fixed it, but may not be the right solution

--- change_varnames_to_vartitles.R
+++ change_varnames_to_vartitles.R
@@ -41,7 +41,7 @@ change_varnames_vartitles <- function(longtable, varnames, vars, ignore_size_war
     dplyr::mutate(ROW_ID = 1:nrow(longtable)) %>% 
     
     #' gather long table with all variables that need to be changed
-    tidyr::gather("VARNAME", "VALUE", vars) %>% 
+    tidyr::gather("VARNAME", "VALUE", !! vars) %>% 
     
     #' separate the _value temporarliy
     tidyr::separate(VARNAME, into = c("VARNAME", "extra_temp"), sep = "_", remove=T) %>% 
