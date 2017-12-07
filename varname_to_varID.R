68f33ef5ee68daf3a4262f4d723171b57f7b695f
Emma Morgan <emma.morgan@tufts.edu>
2017-12-07 14:51:08 -0500

    Merging added file trying to convert varname to varID to reduce problems with duplicate varname in SFA

---
+++ varname_to_varID.R
@@ -0,0 +1,106 @@
+
+
+#Sample script to compile vartable and valuesets longitudinal files
+
+script_vartable <- RCurl::getURL("https://raw.githubusercontent.com/emmamorgan-tufts/IPEDS_longitudinal/master/compile_varname_valueset.R", ssl.verifypeer = FALSE)
+script_peerList <- RCurl::getURL("https://raw.githubusercontent.com/emmamorgan-tufts/IPEDS_longitudinal/master/peerList.R", ssl.verifypeer = FALSE)
+script_filename_to_tablename <- RCurl::getURL("https://raw.githubusercontent.com/emmamorgan-tufts/IPEDS_longitudinal/master/filename_to_tablename.R", ssl.verifypeer = FALSE)
+script_add_valuesets <- RCurl::getURL("https://raw.githubusercontent.com/emmamorgan-tufts/IPEDS_longitudinal/master/add_valuesets.R", ssl.verifypeer = FALSE)
+script_varnames_to_titles <- RCurl::getURL("https://raw.githubusercontent.com/emmamorgan-tufts/IPEDS_longitudinal/master/change_varnames_to_vartitles.R", ssl.verifypeer = FALSE)
+script_acadyear <- RCurl::getURL("https://raw.githubusercontent.com/emmamorgan-tufts/IPEDS_longitudinal/master/acad_yr_function.R", ssl.verifypeer = FALSE)
+
+
+eval(parse(text = script_vartable))
+eval(parse(text = script_peerList))
+eval(parse(text = script_filename_to_tablename))
+eval(parse(text = script_add_valuesets))
+eval(parse(text = script_varnames_to_titles))
+eval(parse(text = script_acadyear))
+
+
+rm("script_vartable","script_peerList","script_filename_to_tablename",
+   "script_add_valuesets","script_varnames_to_titles","pkg","pkgs","script_acadyear")
+
+valuesets_filepath <- "Q:/Staff/University-Wide/Peer Comparison Database/IPEDS/Access Database/Compiling var info/R export/valuesets_compiled_rev.csv"
+vartable_filepath <- "Q:/Staff/University-Wide/Peer Comparison Database/IPEDS/Access Database/Compiling var info/R export/vartable_compiled_rev_EMedits.csv"
+
+valuesets_full <- read.csv(valuesets_filepath,stringsAsFactors = F)
+vartable_full <- read.csv(vartable_filepath,stringsAsFactors = F)
+vartable_full$TABLE_TRIM <- table_from_column(vartable_full$TABLENAME)
+ipeds_tables <- unique(vartable_full[,c("TABLE_TRIM", "SURVEY")])
+
+#Admissions and Test Scores
+survey_path <- "Q:\\Staff\\University-Wide\\Peer Comparison Database\\IPEDS\\Original IPEDS Data\\Admissions & Test Scores"
+
+tableName <- table_from_file(paste(survey_path,"Data",sep="\\"),1)
+
+#Read in all dictionaries first
+
+setwd(paste(survey_path,"Dictionary",sep="\\"))
+dictionary_list <- list()
+
+for (i in 1:length(list.files())) {
+  fileName <- list.files()[i]
+  if ("varlist" %in% readxl::excel_sheets(fileName)) {
+    var_sheet <- "varlist"
+  } else {
+    paste("You must choose the varlist from the sheets below")  
+    paste(readxl::excel_sheets(fileName))
+    var_sheet <- readline("What is the name of the varlist sheet? (Type EXACT)")}
+  ds <- readxl::read_excel(fileName, sheet=var_sheet, na = c(".", "", " ", NA))
+  names(ds) <- toupper(names(ds))
+  #call function to trim dates out of csv filename -- create Table Name
+  ds$TABLE_TRIM <- tableName
+  #join "SURVEY" field in from ipeds_tables, and then un-factorize SURVEY
+  ds <- dplyr::left_join(ds, ipeds_tables, by = "TABLE_TRIM")
+  SURVEY <- as.character(first(ds$SURVEY))
+  # call adacemic year function
+  ay <- acad_year(fileName, SURVEY)
+  ds$ACAD_YEAR <- ay
+  
+  #Add VARIABLE_ID which will then be added to the data set to reduce confusion
+  ds[["VARIABLE_ID"]] <- paste(ds[['VARNAME']],ds[['VARNUMBER']],sep="_")
+  #store each ds in a list with year as name, so we can match with data
+  dictionary_list[[as.character(ay)]] <- assign(paste("ds",i,sep=""), ds)
+}
+
+
+ds_list <- list()
+
+setwd(paste(survey_path,"Data",sep="\\"))
+
+#Borrowed from Kathy's ipeds_rowbind.R - this should be functionalized
+for (i in 1:length(list.files())) {
+  fileName <- list.files()[i]
+  ds_orig <- read.csv(fileName, check.names=FALSE, stringsAsFactors = F, na.strings = c(".", "", " ", NA))
+  names(ds_orig) <- toupper(names(ds_orig))
+  #Remove imputed variables
+  ds_clean <- select(ds_orig, -starts_with("X"))
+  # call adacemic year function
+  ay <- acad_year(fileName, SURVEY)
+  #Convert VARNAME to VARIABLE_ID
+  
+  dict <- dictionary_list[[as.character(ay)]]
+  vars <- dict$VARNAME[2:nrow(dict)]
+  ds <- ds_clean %>%
+    dplyr::mutate(ROW_ID=1:nrow(ds_clean)) %>%
+    tidyr::gather("VARNAME","VALUE",!!vars) %>%
+    dplyr::left_join(select(dict, "VARNAME","VARIABLE_ID")) %>%
+    dplyr::select(-VARNAME) %>%
+    tidyr::spread(key=VARIABLE_ID,value=VALUE) %>%
+    dplyr::select(-ROW_ID)
+  
+  
+  #call function to trim dates out of csv filename -- create Table Name
+  ds$TABLE_TRIM <- tableName
+  #join "SURVEY" field in from ipeds_tables, and then un-factorize SURVEY
+  ds <- dplyr::left_join(ds, ipeds_tables, by = "TABLE_TRIM")
+  SURVEY <- as.character(first(ds$SURVEY))
+
+  ds$ACAD_YEAR <- ay
+  #store each ds in a list
+  ds_list[[as.character(ay)]] <- assign(paste("ds",i,sep=""), ds)
+}
+
+#row bind the ds's together
+full_ds <- dplyr::bind_rows(ds_list)
