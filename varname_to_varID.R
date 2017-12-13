64979b0ecbc2e5204d9b21e4c6ef087b1ffbe861
Emma Morgan <emma.morgan@tufts.edu>
2017-12-13 13:55:41 -0500

    Adding function to compile dictionary for a single survey data set; this will replace the full compiled file; similar method can be used for valuests, though care will need to be taken to avoid issues with VARTITLES that include years rather than current year, prior year, etc.

--- varname_to_varID.R
+++ varname_to_varID.R
@@ -1,12 +1,13 @@
 
 
 #Sample script to compile vartable and valuesets longitudinal files
+#Can we do this without compiling the vartable/valuesets at the beginning?
 
-script_vartable <- RCurl::getURL("https://raw.githubusercontent.com/emmamorgan-tufts/IPEDS_longitudinal/master/compile_varname_valueset.R", ssl.verifypeer = FALSE)
-script_peerList <- RCurl::getURL("https://raw.githubusercontent.com/emmamorgan-tufts/IPEDS_longitudinal/master/peerList.R", ssl.verifypeer = FALSE)
+#script_vartable <- RCurl::getURL("https://raw.githubusercontent.com/emmamorgan-tufts/IPEDS_longitudinal/master/compile_varname_valueset.R", ssl.verifypeer = FALSE)
+#script_peerList <- RCurl::getURL("https://raw.githubusercontent.com/emmamorgan-tufts/IPEDS_longitudinal/master/peerList.R", ssl.verifypeer = FALSE)
 script_filename_to_tablename <- RCurl::getURL("https://raw.githubusercontent.com/emmamorgan-tufts/IPEDS_longitudinal/master/filename_to_tablename.R", ssl.verifypeer = FALSE)
-script_add_valuesets <- RCurl::getURL("https://raw.githubusercontent.com/emmamorgan-tufts/IPEDS_longitudinal/master/add_valuesets.R", ssl.verifypeer = FALSE)
-script_varnames_to_titles <- RCurl::getURL("https://raw.githubusercontent.com/emmamorgan-tufts/IPEDS_longitudinal/master/change_varnames_to_vartitles.R", ssl.verifypeer = FALSE)
+#script_add_valuesets <- RCurl::getURL("https://raw.githubusercontent.com/emmamorgan-tufts/IPEDS_longitudinal/master/add_valuesets.R", ssl.verifypeer = FALSE)
+#script_varnames_to_titles <- RCurl::getURL("https://raw.githubusercontent.com/emmamorgan-tufts/IPEDS_longitudinal/master/change_varnames_to_vartitles.R", ssl.verifypeer = FALSE)
 script_acadyear <- RCurl::getURL("https://raw.githubusercontent.com/emmamorgan-tufts/IPEDS_longitudinal/master/acad_yr_function.R", ssl.verifypeer = FALSE)
 
 
@@ -21,22 +22,52 @@ eval(parse(text = script_acadyear))
 rm("script_vartable","script_peerList","script_filename_to_tablename",
    "script_add_valuesets","script_varnames_to_titles","pkg","pkgs","script_acadyear")
 
-valuesets_filepath <- "Q:/Staff/University-Wide/Peer Comparison Database/IPEDS/Access Database/Compiling var info/R export/valuesets_compiled_rev.csv"
-vartable_filepath <- "Q:/Staff/University-Wide/Peer Comparison Database/IPEDS/Access Database/Compiling var info/R export/vartable_compiled_rev_EMedits.csv"
+#valuesets_filepath <- "Q:/Staff/University-Wide/Peer Comparison Database/IPEDS/Access Database/Compiling var info/R export/valuesets_compiled_rev.csv"
+#vartable_filepath <- "Q:/Staff/University-Wide/Peer Comparison Database/IPEDS/Access Database/Compiling var info/R export/vartable_compiled_rev_EMedits.csv"
 
-valuesets_full <- read.csv(valuesets_filepath,stringsAsFactors = F)
-vartable_full <- read.csv(vartable_filepath,stringsAsFactors = F)
-vartable_full$TABLE_TRIM <- table_from_column(vartable_full$TABLENAME)
-ipeds_tables <- unique(vartable_full[,c("TABLE_TRIM", "SURVEY")])
+# valuesets_full <- read.csv(valuesets_filepath,stringsAsFactors = F)
+# vartable_full <- read.csv(vartable_filepath,stringsAsFactors = F)
+# vartable_full$TABLE_TRIM <- table_from_column(vartable_full$TABLENAME)
+# ipeds_tables <- unique(vartable_full[,c("TABLE_TRIM", "SURVEY")])
+
+compile_dict_unique <- function(dictionary_list) {
+  #Check to make sure we have necessary columns
+  dictionary_full <- dplyr::bind_rows(dictionary_list)
+  if (! all(c("VARNAME","VARTITLE","VARIABLE_ID","TABLE_TRIM","ACAD_YEAR","FILENAME") %in% names(dictionary_full))) {
+    stop("vartable does not have required columns from dictionary files; please fix your data frame and try again")
+  }
+  
+  #Sort so that we have the most recent year LAST; this will keep the most recent version of the variable
+  dictionary_sorted <- dictionary_full[order(-dictionary_full$ACAD_YEAR, dictionary_full$VARIABLE_ID),]
+  
+  dictionary_unique <- dictionary_sorted[!duplicated(dictionary_sorted['VARIABLE_ID'], fromLast = FALSE),]
+  
+  #Check to see if we have any repeated VARTITLE
+  
+  duplicate_vartitles <- which(duplicated(dictionary_unique['VARTITLE'], fromLast=FALSE) |
+                                 duplicated(dictionary_unique['VARTITLE'], fromLast=TRUE))
+  
+  dictionary_unique[['VARTITLE_USE']] <- dictionary_unique[['VARTITLE']]
+  
+  
+  
+  if (length(duplicate_vartitles) > 0 ){
+    dictionary_unique[['VARTITLE_USE']][duplicate_vartitles] <- sapply(duplicate_vartitles, 
+                                                                       function(x) dictionary_unique[['VARTITLE_USE']][x] <- 
+                                                                         paste(dictionary_unique[['VARTITLE_USE']][x]," 
+                                                                        (",dictionary_unique[['VARNAME']][x],")",sep=""))
+  }
+}
+
 
 #Admissions and Test Scores
-survey_path <- "Q:\\Staff\\University-Wide\\Peer Comparison Database\\IPEDS\\Original IPEDS Data\\Admissions & Test Scores"
-
-tableName <- table_from_file(paste(survey_path,"Data",sep="\\"),1)
+IPEDS_data_location <- "Q:\\Staff\\University-Wide\\Peer Comparison Database\\IPEDS\\Original IPEDS Data"
+surveyName <- "Admissions"
+tableName <- table_from_file(paste(IPEDS_data_location, surveyName, "Data",sep="\\"),1)
 
 #Read in all dictionaries first
 
-setwd(paste(survey_path,"Dictionary",sep="\\"))
+setwd(paste(IPEDS_data_location, surveyName,"Dictionary",sep="\\"))
 dictionary_list <- list()
 
 for (i in 1:length(list.files())) {
@@ -52,11 +83,13 @@ for (i in 1:length(list.files())) {
   #call function to trim dates out of csv filename -- create Table Name
   ds$TABLE_TRIM <- tableName
   #join "SURVEY" field in from ipeds_tables, and then un-factorize SURVEY
-  ds <- dplyr::left_join(ds, ipeds_tables, by = "TABLE_TRIM")
-  SURVEY <- as.character(first(ds$SURVEY))
+  #Changed this - we shouldn't have to merge in Survey; instead use the folder structure of the data?
+#REMOVE::  ds <- dplyr::left_join(ds, ipeds_tables, by = "TABLE_TRIM")
+#REMOVE::  SURVEY <- as.character(first(ds$SURVEY))
   # call adacemic year function
-  ay <- acad_year(fileName, SURVEY)
+  ay <- acad_year(fileName, surveyName)
   ds$ACAD_YEAR <- ay
+  ds[['FILENAME']] <- fileName
   
   #Add VARIABLE_ID which will then be added to the data set to reduce confusion
   ds[["VARIABLE_ID"]] <- paste(ds[['VARNAME']],ds[['VARNUMBER']],sep="_")
@@ -64,10 +97,12 @@ for (i in 1:length(list.files())) {
   dictionary_list[[as.character(ay)]] <- assign(paste("ds",i,sep=""), ds)
 }
 
+#Create compiled survey dictionary
+#Use list of dictionary data files for current year to make this happen
 
 ds_list <- list()
 
-setwd(paste(survey_path,"Data",sep="\\"))
+setwd(paste(IPEDS_data_location, surveyName,"Data",sep="\\"))
 
 #Borrowed from Kathy's ipeds_rowbind.R - this should be functionalized
 for (i in 1:length(list.files())) {
@@ -77,7 +112,7 @@ for (i in 1:length(list.files())) {
   #Remove imputed variables
   ds_clean <- select(ds_orig, -starts_with("X"))
   # call adacemic year function
-  ay <- acad_year(fileName, SURVEY)
+  ay <- acad_year(fileName, surveyName)
   #Convert VARNAME to VARIABLE_ID
   
   dict <- dictionary_list[[as.character(ay)]]
