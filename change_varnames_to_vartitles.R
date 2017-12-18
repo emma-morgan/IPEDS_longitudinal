c5fd57fa89fdac88b6f2e45b320bd2149f7ea865
Emma Morgan <emma.morgan@tufts.edu>
2017-12-18 12:47:15 -0500

    Adjusting change_varnames_to_vartitles.R to try and use now whiel full code is being developed for new process

--- change_varnames_to_vartitles.R
+++ change_varnames_to_vartitles.R
@@ -1,6 +1,7 @@
 #' Goal: Change variable names to more english words.
 #' Contains function input dataset longitudinal table file and variable name file output  dataset longitudinal table file with variable titles instead of variable name 
 #' KMA - 8/10/17
+#' EM - 12/18/17 (added to see if I can make it work for me!)
 #' 
 #' 
 #' Requires one longitudinal table and a premade subset of varname with only variables from longitudinal table
@@ -42,12 +43,12 @@ change_varnames_vartitles <- function(longtable, varnames, ignore_size_warning =
   if(ignore_size_warning){warning("Large file may break things, consider using subset_peerlist() to reduce file size and compile time.")}
   
   # XX THIS SHOULD NOT BE HERE THIS SHOULD BE RUN IN COMPILING VARNAMES BUT IT IS HERE NOW SO THIS WORKS
-  varnames$TABLE_TRIM <- table_from_column(varnames$TABLENAME)
+#  varnames$TABLE_TRIM <- table_from_column(varnames$TABLENAME)
   
   # filter to only varnames that are in longtable based on table trim - this is problematic if the trim file name is different than the trim name in varnames
-  varnames <- varnames %>% filter(varnames$TABLE_TRIM%in%longtable$TABLE_TRIM)
+#  varnames <- varnames %>% filter(varnames$TABLE_TRIM%in%longtable$TABLE_TRIM)
   vars <- list()
-  for (name in names(longtable)[names(longtable)%in%varnames$VARNAME]) vars[[name]] <- names(longtable)[grepl(paste0(name, "_*"), names(longtable))]
+  for (name in names(longtable)[names(longtable)%in%varnames$VARIABLE_ID]) vars[[name]] <- names(longtable)[grepl(paste0(name, "_*"), names(longtable))]
   vars <- unlist(vars, use.names = F)
   
   ds <- longtable %>% 
@@ -56,14 +57,14 @@ change_varnames_vartitles <- function(longtable, varnames, ignore_size_warning =
     dplyr::mutate(ROW_ID = 1:nrow(longtable)) %>% 
     
     #' gather long table with all variables that need to be changed, previous versions of tidyr need to have !!var in order to work
-    tidyr::gather("VARNAME", "VALUE", !!vars) %>% 
+    tidyr::gather("VARIABLE_ID", "VALUE", !!vars) %>% 
     
     #' separate the _value temporarliy
-    tidyr::separate(VARNAME, into = c("VARNAME", "extra_temp"), sep = "_val", remove=T) %>% 
+    tidyr::separate(VARIABLE_ID, into = c("VARIABLE_ID", "extra_temp"), sep = "_val", remove=T) %>% 
     
     #' left join with clean var title
     dplyr::left_join(select(varnames,
-                     "VARNAME",
+                     "VARIABLE_ID",
                      "VARTITLE_USE")) %>% 
     
     #' re paste the _value column
@@ -71,7 +72,7 @@ change_varnames_vartitles <- function(longtable, varnames, ignore_size_warning =
                                    paste0(VARTITLE_USE, "_val", extra_temp), VARTITLE_USE)) %>%   
     
     #'remove unneeded variables for spread to be happy
-    dplyr::select(-VARTITLE_USE, -extra_temp, -VARNAME) %>% 
+    dplyr::select(-VARTITLE_USE, -extra_temp, -VARIABLE_ID) %>% 
     
     #' rename vartitle_clean to be consistant
     dplyr::rename(VARTITLE = VARTITLE_CLEAN) %>% 
