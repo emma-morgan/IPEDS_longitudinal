147c547b9a48bf12348243259616338ab037a81a
Kathleen Foley <kfoley@smith.edu>
2017-12-18 09:18:49 -0500

    added more table_trims to list

--- acad_yr_function.R
+++ acad_yr_function.R
@@ -22,8 +22,10 @@ acad_year <- function(fileName, TABLE_TRIM){
                stringr::str_extract(fileName, '[[:digit:]]{2,4}'))
   yr <- ifelse(nchar(yr)==2, str_pad(yr,4,side="left", pad="0"), yr)
   yr <- stringr::str_replace(yr, str_sub(yr, 1,2), "20")
-  acadyr <- ifelse(TABLE_TRIM %in% c("ADM", "EFA", "EFB", "EFC", "EFD", "EFCP" , "IC", "IC_AY", "IC_PY",
-                       "S_F", "S_IS", "S_NH", "S_OC", "S_SIS"), (as.integer(yr) +1), yr)
+  acadyr <- ifelse(TABLE_TRIM %in% c("ADM", "DRVADM", "EF", "EFA", "EFB", "EFC", "EFD", "EFCP", "EFA_DIST", "DRVEF", 
+                       "HD","IC", "IC_AY", "IC_PY", "DRVIC", "ICMISSION", "CUSTOMCGIDS",
+                       "EAP", "SAL_B", "SAL_A", "SAL_IS", "SAL_FACULTY", "SAL_NIS", "SAL_A_LT9",
+                      "S_OC", "S_ABD", "S_SIS", "S_F", "S_IS", "S_G", "S_NH", "S_CN", "DRVHR"), (as.integer(yr) +1), yr)
   return(acadyr)
 }
 
