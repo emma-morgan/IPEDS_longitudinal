215c2259e242b07b667afaaca57f8bd1ef7d81df
Emma Morgan <emma.morgan@tufts.edu>
2017-12-19 13:55:45 -0500

    Adding option to add vartitles and valuelabels without subsetting first; used to test finance data

--- startup_em.R
+++ startup_em.R
@@ -1,15 +1,15 @@
 #Updated by Emma Morgan (emma.morgan@tufts.edu)
-#12/18/2017
+#12/19/2017
 
 #Sample script to compile vartable and valuesets longitudinal files
 #Can we do this without compiling the vartable/valuesets at the beginning?
 
 script_peerList <- RCurl::getURL("https://raw.githubusercontent.com/emmamorgan-tufts/IPEDS_longitudinal/master/peerList.R", ssl.verifypeer = FALSE)
 script_filename_to_tablename <- RCurl::getURL("https://raw.githubusercontent.com/emmamorgan-tufts/IPEDS_longitudinal/master/filename_to_tablename.R", ssl.verifypeer = FALSE)
-script_add_valuesets <- RCurl::getURL("https://raw.githubusercontent.com/emmamorgan-tufts/IPEDS_longitudinal/develop_em/add_valuesets.R", ssl.verifypeer = FALSE)
-script_varnames_to_titles <- RCurl::getURL("https://raw.githubusercontent.com/emmamorgan-tufts/IPEDS_longitudinal/develop_em/change_varnames_to_vartitles.R", ssl.verifypeer = FALSE)
+script_add_valuesets <- RCurl::getURL("https://raw.githubusercontent.com/emmamorgan-tufts/IPEDS_longitudinal/master/add_valuesets.R", ssl.verifypeer = FALSE)
+script_varnames_to_titles <- RCurl::getURL("https://raw.githubusercontent.com/emmamorgan-tufts/IPEDS_longitudinal/master/change_varnames_to_vartitles.R", ssl.verifypeer = FALSE)
 script_acadyear <- RCurl::getURL("https://raw.githubusercontent.com/emmamorgan-tufts/IPEDS_longitudinal/master/acad_yr_function.R", ssl.verifypeer = FALSE)
-script_varname_to_varID <- RCurl::getURL("https://raw.githubusercontent.com/emmamorgan-tufts/IPEDS_longitudinal/develop_em/varname_to_varID.R", ssl.verifypeer = FALSE)
+script_varname_to_varID <- RCurl::getURL("https://raw.githubusercontent.com/emmamorgan-tufts/IPEDS_longitudinal/master/varname_to_varID.R", ssl.verifypeer = FALSE)
 
 
 eval(parse(text = script_peerList))
@@ -21,7 +21,8 @@ eval(parse(text = script_varname_to_varID))
 
 
 rm("script_peerList","script_filename_to_tablename","script_acadyear",
-   "script_add_valuesets", "script_varname_to_varID","script_varnames_to_titles","pkg","pkgs")
+   "script_add_valuesets", "script_varname_to_varID","script_varnames_to_titles",
+   "pkg","pkgs")
 
 ########TEST###########################
 
@@ -55,6 +56,13 @@ data_final <- dplyr::left_join(data_add_vartitles, peerList$peerdf,"UNITID","INS
 output_dir <- "Q:/Staff/President, Provost, Trustees/TAAC Dashboard/Data/AY 2017-18 TAAC DB Data/Adm Enroll Grad Fin Aid (Feb 18)/IPEDS Peer Comparison"
 
 data.table::fwrite(data_final,paste(output_dir,"/",surveyFolder,"_",Sys.Date(),".csv",sep=""))
+
+##Testing for Dana without subset
+#If we're testing with no subset...
+data_add_valuesets <- add_values(longtable=IPEDS_data, valueset = IPEDS_valueset)
+data_add_vartitles <- change_varnames_vartitles(longtable=data_add_valuesets, varnames=IPEDS_dictionary)
+output_dir <- "P:/Peer comparison Database/IPEDS database/For Dana Aug 2017"
+data.table::fwrite(data_add_vartitles,paste(output_dir,"/",surveyFolder,"_",Sys.Date(),".csv",sep=""))
 
 #Admissions
 surveyFolder <- "Admissions"
