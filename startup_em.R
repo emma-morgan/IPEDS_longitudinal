859a7993527736c03f3b6a690a6352b7c68d0e05
Emma Morgan <emma.morgan@tufts.edu>
2017-12-20 14:45:20 -0500

    Updating Emma's testing document with alterations and tests from 12/20/2017

--- startup_em.R
+++ startup_em.R
@@ -29,7 +29,8 @@ rm("script_peerList","script_filename_to_tablename","script_acadyear",
 #Run this line to clear everything except functions
 rm("data_add_valuesets","data_add_vartitles","data_final","IPEDS_data",
    "IPEDS_data_subset","IPEDS_dictionary","IPEDS_valueset","IPEDS_data_location",
-   "IPEDS_data_location_general","IPEDS_test","peer_filepath","peerList","surveyFolder")
+   "IPEDS_data_location_general","IPEDS_test","peer_filepath","peerList","surveyFolder",
+   "output_dir","IPEDS_data_Carnegie")
 
 #surveyFolder <- 
 IPEDS_data_location_general <- "Q:\\Staff\\University-Wide\\Peer Comparison Database\\IPEDS\\Original IPEDS Data"
@@ -59,7 +60,7 @@ data.table::fwrite(data_final,paste(output_dir,"/",surveyFolder,"_",Sys.Date(),"
 
 ##Testing for Dana without subset
 #If we're testing with no subset...
-data_add_valuesets <- add_values(longtable=IPEDS_data, valueset = IPEDS_valueset)
+data_add_valuesets <- add_values(longtable=IPEDS_data, valueset = IPEDS_valueset, ignore_size_warning = T)
 data_add_vartitles <- change_varnames_vartitles(longtable=data_add_valuesets, varnames=IPEDS_dictionary)
 output_dir <- "P:/Peer comparison Database/IPEDS database/For Dana Aug 2017"
 data.table::fwrite(data_add_vartitles,paste(output_dir,"/",surveyFolder,"_",Sys.Date(),".csv",sep=""))
@@ -79,6 +80,36 @@ surveyFolder <- "Graduation Rates"
 #Student Financial Aid
 surveyFolder <- "Student Financial Aid"
 
+#Directory Information (IC Header File)
+surveyFolder <- "Directory Information"
+
+  #Subset to include only the most recent year - we want each entry only once!
+directory_info_recent <- IPEDS_data[IPEDS_data$ACAD_YEAR==max(IPEDS_data$ACAD_YEAR),]
+naCols <- names(which(sapply(directory_info_recent, function(x) all(is.na(x)))))
+directory_info_recent_CLEAN <- dplyr::select(directory_info_recent, -which(names(directory_info_recent) %in% naCols))
+data_add_valuesets <- add_values(longtable=directory_info_recent_CLEAN, valueset = IPEDS_valueset)
+data_add_vartitles <- change_varnames_vartitles(longtable=data_add_valuesets, varnames=IPEDS_dictionary)
+
+output_dir <- "Q:/Staff/University-Wide/Peer Comparison Database/IPEDS/IPEDS World Domination compiled"
+data.table::fwrite(data_add_vartitles,paste(output_dir,"/",surveyFolder,"_",as.character(max(directory_info_recent$ACAD_YEAR)-1),".csv",sep=""))
+
+#######Preliminary subsetting using directory information###############
+
+headFile <- read.csv("Q:/Staff/University-Wide/Peer Comparison Database/IPEDS/IPEDS World Domination compiled/Directory Information_2016.csv", stringsAsFactors = FALSE)
+
+#Subset to only Bachelors, Masters, and Doctoral institutions; remove associates and special focus
+IPEDS_data_Carnegie <- dplyr::left_join(IPEDS_data, dplyr::select(headFile, "UNITID","Carnegie.Classification.2015..Basic"))
+IPEDS_data_subset <- dplyr::filter(IPEDS_data_Carnegie, 
+                                   #startsWith(x=IPEDS_data_Carnegie$Carnegie.Classification.2015..Basic, prefix="Bacc") |
+                                    # startsWith(x=IPEDS_data_Carnegie$Carnegie.Classification.2015..Basic, prefix="Mast") |
+                                     startsWith(x=IPEDS_data_Carnegie$Carnegie.Classification.2015..Basic, prefix="Doct")
+                                   )
+
+data_add_valuesets <- add_values(longtable=IPEDS_data_subset, valueset = IPEDS_valueset)
+data_add_vartitles <- change_varnames_vartitles(longtable=data_add_valuesets, varnames=IPEDS_dictionary)
+output_dir <- "Q:/Staff/University-Wide/Peer Comparison Database/IPEDS/IPEDS World Domination compiled"
+data.table::fwrite(data_add_vartitles,paste(output_dir,"/",surveyFolder,".csv",sep=""))
+data.table::fwrite(data_add_vartitles,paste(output_dir,"/","Dated files","/",surveyFolder,"_",Sys.Date(),".csv",sep=""))
 
 #Possible Survey Names:
 
