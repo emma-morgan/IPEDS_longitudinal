331cfa43e3dd97b11e7919ff2800edcf985541ae
Morgan <emorga06@tufts.edu>
2019-05-21 13:30:51 -0400

    updating lookup_helper in develop_em to match master

--- lookup_helper.R
+++ lookup_helper.R
@@ -25,7 +25,6 @@ eval(parse(text = script_filename_to_tablename))
 eval(parse(text = script_acadyear))
 eval(parse(text = script_dictionary_years_to_index))
 rm("script_filename_to_tablename","script_acadyear","script_dictionary_years_to_index")
->>>>>>> origin/master
 
 #Given your data location and survey name, this function will return
 # a unique compiled dictionary and a list of dictionary dfs indexed by AY
@@ -110,13 +109,13 @@ lookup_unique <- function(lookup_list, sheetName) {
                                                                   lookup_unique[['LOOKUP_ID']][x],")",sep=""))
   }
   names(lookup_unique)[which(names(lookup_full)=="LOOKUP_ID")] <- lookup_col
-
+  
   #Replace stated academic years to year indices in the vartitles; this currently is necessary
     #only for Student Financial Aid (SFA) and Student Charges (IC_AY)
   if (sheetName == "varlist" & unique(lookup_unique$TABLE_TRIM) %in% c("SFA", "IC_AY")) {
     lookup_unique <- dictionary_years_to_index(lookup_unique)
   }
   
-
+  
   return (lookup_unique)
 }
