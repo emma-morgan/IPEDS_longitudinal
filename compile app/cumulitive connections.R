a9043651ae1d04bd13bc70ccf6c2888994211d46
Kate Aloisio <kaloisio@gmail.com>
2020-08-03 14:18:16 -0400

    updated to include a graph for todays number of connections

--- compile app/cumulitive connections.R
+++ compile app/cumulitive connections.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(lubridate)
+library(rsconnect)
 
 # http://docs.rstudio.com/shinyapps.io/metrics.html#ApplicationMetrics
 df <- rsconnect::showMetrics("container_status",
@@ -12,7 +13,7 @@ df <- rsconnect::showMetrics("container_status",
 ) 
 
 df1 <- df %>% 
-  mutate(date=as_datetime(timestamp)) %>% 
+  mutate(date=as_datetime(timestamp, tz="EST")) %>% 
   select(-timestamp) %>% 
   arrange(date) %>% 
   mutate(
@@ -35,3 +36,13 @@ p2 <- ggplot(df2) +
   facet_wrap(~key)
 
 print(p2)
+
+today <- df2 %>% filter(date>=ymd(today(),tz = "EDT")) %>% 
+  mutate(value_new = value-min(value))
+
+ptoday <- ggplot(today) +
+  labs(title=paste0("Number of Connections Today: ", today()), x="", y="") +
+  geom_line(aes(x=date, y=value_new, colour=key)) +
+  facet_wrap(~key)
+
+print(ptoday)
