b0c8148d7f4d9d7c9ecdfa173391345e777cb05f
Kate Aloisio <kaloisio@gmail.com>
2020-10-08 14:30:05 -0400

    updated faq and republished

--- compile app/app.R
+++ compile app/app.R
@@ -283,8 +283,8 @@ br(),
                                             "https://nces.ed.gov/ipeds/use-the-data/survey-components",
                                             target = "blank"))) %>% 
       
-      bs_append("Does the app use Prelimiarny, Provisional, or Final IPEDS data?", "Our app provides Provisional data when it is avalable and Final data otherwise.  The app never uses Preliminary data." ) %>% 
-      bs_append("Does subsequent revisions of IPEDS data get picked up by the app?", "The app uses revised IPEDS files when they are avalable.  We update the app with new IPEDS data twice a year (fall and winter).  At these times, we check for both new data and revised versions of previous data files." ), 
+      bs_append("Does the app use Preliminary, Provisional, or Final IPEDS data?", "The app provides Final data and Revised Final data.  The app does not provide Preliminary or Provisional data." ) %>% 
+      bs_append("Does subsequent revisions of IPEDS data get picked up by the app?", "The app uses revised IPEDS files when they are available.  We update the app with new IPEDS data twice a year (fall and winter).  At these times, we check for both new final data and revised versions of previous final data files." ), 
       
       
       ) # closes tabpanel
