01297d55016c07757bc2bcba8c4d754fceae9cea
Emma Morgan <emm.c.morgan@gmail.com>
2021-03-29 21:23:17 -0400

    specifying stringr call from acad_yr_function file

--- acad_yr_function.R
+++ acad_yr_function.R
@@ -22,7 +22,7 @@ acad_year <- function(fileName, TABLE_TRIM){
   yr <- ifelse(grepl("200_", fileName), stringr::str_extract(fileName, '(?<=\\_)[[:digit:]]{2,4}'),  
                stringr::str_extract(fileName, '[[:digit:]]{2,4}'))
   yr <- ifelse(nchar(yr)==2, str_pad(yr,4,side="left", pad="0"), yr)
-  yr <- stringr::str_replace(yr, str_sub(yr, 1,2), "20")
+  yr <- stringr::str_replace(yr, stringr::str_sub(yr, 1,2), "20")
   acadyr <- ifelse(TABLE_TRIM %in% c("ADM", "DRVADM", "EF", "EFA", "EFB", "EFC", "EFD", "EFCP", "EFA_DIST", "DRVEF", 
                        "HD","IC", "IC_AY", "IC_PY", "DRVIC", "ICMISSION", "CUSTOMCGIDS",
                        "EAP", "SAL_B", "SAL_A", "SAL_IS", "SAL_FACULTY", "SAL_NIS", "SAL_A_LT9",
