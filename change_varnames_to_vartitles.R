4806c7af067f0777a0d84c05861c07607f03b990
Kate Aloisio <kaloisio@gmail.com>
2023-07-12 15:47:07 -0400

    Updated 7/12/2023 with the help of Mark and Srishti to make more efficient

--- change_varnames_to_vartitles.R
+++ change_varnames_to_vartitles.R
@@ -2,6 +2,7 @@
 #' Contains function input dataset longitudinal table file and variable name file output  dataset longitudinal table file with variable titles instead of variable name 
 #' KMA - 8/10/17
 #' KMA - Updated 3/24/2023 to replace gather and spread
+#' KMA - Updated 7/12/2023 with the help of Mark and Srishti to make more efficent
 #' 
 #' Requires one longitudinal table and a premade subset of varname with only variables from longitudinal table
 #' Assumes data file has already been run through add_value function
@@ -27,47 +28,41 @@ change_varnames_vartitles <- function(longtable, varnames, ignore_size_warning =
   if(ignore_size_warning){warning("Large file may break things, consider using subset_peerlist() to reduce file size and compile time.")}
   
   
-  # filter to only varnames that are in longtable based on dictionary VARIABLE_ID
-  # XX I DONT KNOW IF THIS LINE IS NECESSARY IF SOMETHING BREAKS CHECK THIS LINE FIRST
-  #varnames <- varnames %>% filter(varnames$TABLE_TRIM%in%longtable$TABLE_TRIM)
-  
-  
-  vars <- list()
-  for (name in names(longtable)[names(longtable)%in%varnames$VARIABLE_ID]) vars[[name]] <- names(longtable)[grepl(paste0(name, "_*"), names(longtable))]
-  vars <- unlist(vars, use.names = F)
-  
   #'because of multiple years need to add a unique row id for spread to work
   ds <- dplyr::mutate(longtable, ROW_ID = 1:nrow(longtable))
   
-  #' gather long table with all variables that need to be changed, previous versions of tidyr need to have !!var in order to work
-  ds <- tidyr::pivot_longer(ds, cols=!!vars, names_to = "VARIABLE_ID", values_to = "VALUE")
-  #ds <- tidyr::gather(ds, "VARIABLE_ID", "VALUE", !!vars)
+  #' gather long table with all variables that need to be changed
+  ds <- tidyr::pivot_longer(ds, 
+                            cols = colnames(ds %>% 
+                                              select(-ROW_ID, -UNITID, 
+                                                     -ACAD_YEAR, -FILE_NAME, 
+                                                     -TABLE_TRIM)), 
+                            names_to = "VARIABLE_ID", 
+                            values_to = "VALUE")
   
-  #' separate the _value temporarily
-  ds <- tidyr::separate(ds, VARIABLE_ID, into = c("VARIABLE_ID", "extra_temp"), sep = "_val", remove=T)
+  #' create a copy of the variable id with no _value in order to merge with IPEDS dictionary
+  ds <- ds %>% mutate(VARIABLE_ID_noval = gsub("_value", "", VARIABLE_ID))
+  
   
   #' left join with clean var title
-  ds <- dplyr::left_join(ds, select(varnames,
-                                    "VARIABLE_ID",
-                                    "VARTITLE_USE"))
+  ds <- ds %>% left_join(varnames %>% 
+                           select(VARIABLE_ID, 
+                                  VARTITLE_USE), by=c("VARIABLE_ID_noval"="VARIABLE_ID"))
   
-  ds <- dplyr::mutate(ds, VARTITLE_CLEAN = ifelse(!is.na(extra_temp),
-                                                    paste0(VARTITLE_USE, "_val", extra_temp), VARTITLE_USE))   
+  #' If original variable name has _value suffix, ensure variable label has _value suffix
+  ds <- ds %>% mutate(VARTITLE = ifelse(VARIABLE_ID != VARIABLE_ID_noval, 
+                      paste0(VARTITLE_USE, "_value"), VARTITLE_USE))
+  
+  
   #'remove unneeded variables for spread to be happy
-  ds <- dplyr::select(ds, -VARTITLE_USE, -extra_temp, -VARIABLE_ID)
-    
-  #' rename vartitle_clean to be consistant
-  ds <- dplyr::rename(ds, VARTITLE = VARTITLE_CLEAN)
+  ds <- dplyr::select(ds, -VARTITLE_USE, -VARIABLE_ID_noval, -VARIABLE_ID)
     
   #' spread to make dataset wide again
-  #ds_old <- tidyr::spread(ds, key = VARTITLE, value = VALUE)
-  #ds <- tidyr::pivot_wider(ds, names_from = VARTITLE, values_from = VALUE) 
   ds <- tidyfast::dt_pivot_wider(ds, names_from = VARTITLE, values_from = VALUE)
   
   #' remove row id
   ds <- dplyr::select(ds, -ROW_ID)
-    
   
-#' return dataset
-return(ds)
+  #' return dataset
+  return(ds)
   }
